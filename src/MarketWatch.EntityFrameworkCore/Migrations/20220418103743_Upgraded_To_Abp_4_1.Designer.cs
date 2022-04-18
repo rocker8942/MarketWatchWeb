78a7eb8f0d9c545364f839d525a76b5800991fec
joe park <rocker8942@gmail.com>
2022-04-18 19:42:41 +0800

    add db migration to Abp4_1

--- src/MarketWatch.EntityFrameworkCore/Migrations/20220418103743_Upgraded_To_Abp_4_1.Designer.cs
+++ src/MarketWatch.EntityFrameworkCore/Migrations/20220418103743_Upgraded_To_Abp_4_1.Designer.cs
@@ -4,6 +4,7 @@ using MarketWatch.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,10 @@ using Volo.Abp.EntityFrameworkCore;
 namespace MarketWatch.Migrations
 {
     [DbContext(typeof(MarketWatchDbContext))]
-    partial class MarketWatchDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220418103743_Upgraded_To_Abp_4_1")]
+    partial class Upgraded_To_Abp_4_1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
